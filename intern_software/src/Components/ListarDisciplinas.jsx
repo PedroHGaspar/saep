c84c93c8de483b63621a2010a48c7c8f63886516
Pedro Gaspar <ordep12@hotmail.com.br>
2023-11-30 20:22:01 -0300

    commit

--- intern_software/src/Components/ListarDisciplinas.jsx
+++ intern_software/src/Components/ListarDisciplinas.jsx
@@ -1,36 +1,26 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import "../style/listarDisciplinas.css";
 
 const ListarDisciplinas = () => {
-  const [disciplinas, setDisciplinas] = useState([]);
+  const [disciplinas, setDisciplinas] = useState([
+    { id_discip: 1, nm_disciplina: "Matemática", qtd_dias: 5, num_fase: 1 },
+    { id_discip: 2, nm_disciplina: "Português", qtd_dias: 4, num_fase: 2 },
+    // Add more disciplines as needed
+  ]);
+
   const [newDisciplina, setNewDisciplina] = useState("");
   const [newQtdDias, setNewQtdDias] = useState("");
   const [newNumFase, setNewNumFase] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedDisciplinas, setSelectedDisciplinas] = useState(null);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-  const [deleteId, setDeleteId] = useState("");
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
-  useEffect(() => {
-    fetchDisciplinas();
-  }, []);
-
-  const fetchDisciplinas = async () => {
-    try {
-      let api = `https://senai-back-end.onrender.com/disciplina/lista`;
-      let response = await fetch(api);
-      const data = await response.json();
-      setDisciplinas(data);
-    } catch (error) {
-      console.error("Deu ruim: ", error);
-    }
-  };
-
-  //Modal de cadastro e edição
   const openModal = () => {
     setIsModalOpen(true);
   };
+
   const closeModal = () => {
     setIsModalOpen(false);
     setSelectedDisciplinas(null);
@@ -38,123 +28,80 @@ const ListarDisciplinas = () => {
     setNewQtdDias("");
     setNewNumFase("");
     setIsDeleteModalOpen(false);
+    setIsEditModalOpen(false);
   };
 
-  //Modal de Delete
   const openDeleteModal = (idDelete) => {
     setIsDeleteModalOpen(true);
-    setDeleteId(idDelete);
+    setSelectedDisciplinas(idDelete);
+  };
+
+  const openEditModal = (disciplina) => {
+    setSelectedDisciplinas(disciplina.id_discip);
+    setNewDisciplina(disciplina.nm_disciplina);
+    setNewQtdDias(disciplina.qtd_dias);
+    setNewNumFase(disciplina.num_fase);
+    setIsEditModalOpen(true);
   };
 
   const handleNameChange = (e) => {
     setNewDisciplina(e.target.value);
   };
+
   const handleQtdDias = (e) => {
     setNewQtdDias(e.target.value);
   };
+
   const handleNumFase = (e) => {
     setNewNumFase(e.target.value);
   };
 
-  //CADASTRO (POST)
-  const handleCadastrar = async () => {
-    const fetchData = async () => {
-      //Gerador de ID que busca o ID mais alto, e adciona +1
-      // const idMaisAlto = Math.max(...disciplinas.disciplinasLista.map(disciplina => disciplina.id_discip));
-      // let id_creator = idMaisAlto + 1;
-
-      // Modifiquei o gerador de ID porque eu tava tentando criar disciplina e não criava pq o id vinha null
-      let id_creator = Math.random().toString(5).slice(2, 7);
-
-      if (id_creator <= 99999) {
-        try {
-          let api = `https://senai-back-end.onrender.com/disciplina/postar`;
-          const body = {
-            id_discip: id_creator,
-            nm_disciplina: `${newDisciplina}`,
-            qtd_dias: newQtdDias,
-            num_fase: newNumFase,
-          };
-          let response = await fetch(api, {
-            method: "POST",
-            body: JSON.stringify(body),
-            headers: {
-              "Content-type": "application/json; charset=UTF-8",
-            },
-          });
-          await response.json();
-          console.log(id_creator);
-        } catch (error) {
-          console.error("Deu ruim: ", error);
-        }
-      } else {
-        console.error("Unable to find a unique ID within the 5-digit limit.");
-      }
-    };
-
-    await fetchData();
-    closeModal();
-    await fetchDisciplinas();
+  const handleCadastrar = () => {
+    if (newDisciplina && newQtdDias && newNumFase) {
+      const newDisciplinaObject = {
+        id_discip: disciplinas.length + 1,
+        nm_disciplina: newDisciplina,
+        qtd_dias: parseInt(newQtdDias),
+        num_fase: parseInt(newNumFase),
+      };
+      setDisciplinas([...disciplinas, newDisciplinaObject]);
+      closeModal();
+    }
   };
 
-  //EXCLUSÃO (DELETE)
-  const handleExcluir = async (id_discip) => {
-    const fetchData = async () => {
-      try {
-        let api = `https://senai-back-end.onrender.com/disciplina/deletar/${id_discip}`;
-        let response = await fetch(api, { method: "DELETE" });
-        await response.json();
-      } catch (error) {
-        console.error("Deu ruim: ", error);
-      }
-    };
-
-    await fetchData();
-    await fetchDisciplinas();
+  const handleExcluir = () => {
+    const updatedDisciplinas = disciplinas.filter(
+      (disciplina) => disciplina.id_discip !== selectedDisciplinas
+    );
+    setDisciplinas(updatedDisciplinas);
     closeModal();
   };
 
-  //EDIÇÃO (PUT)
   const handleEditar = (disciplina) => {
-    setSelectedDisciplinas(disciplina);
-    setNewDisciplina(disciplina.nm_disciplina);
-    setNewQtdDias(disciplina.qtd_dias);
-    setNewNumFase(disciplina.num_fase);
-    openModal();
+    openEditModal(disciplina);
   };
 
-  const handleSalvarEdicao = async (id_discip) => {
-    const fetchData = async () => {
-      try {
-        let api = `https://senai-back-end.onrender.com/disciplina/atualizar/${id_discip}`;
-        let response = await fetch(api, {
-          method: "PUT",
-          body: JSON.stringify({
-            nm_disciplina: `${newDisciplina}`,
-            qtd_dias: newQtdDias,
-            num_fase: newNumFase,
-          }),
-          headers: {
-            "Content-type": "application/json; charset=UTF-8",
-          },
-        });
-        const data = await response.json();
-        setDisciplinas(data);
-      } catch (error) {
-        console.error("Deu ruim: ", error);
+  const handleSalvarEdicao = () => {
+    const updatedDisciplinas = disciplinas.map((disciplina) => {
+      if (disciplina.id_discip === selectedDisciplinas) {
+        return {
+          ...disciplina,
+          nm_disciplina: newDisciplina,
+          qtd_dias: parseInt(newQtdDias),
+          num_fase: parseInt(newNumFase),
+        };
       }
-    };
-
-    await fetchData();
+      return disciplina;
+    });
+    setDisciplinas(updatedDisciplinas);
     closeModal();
-    await fetchDisciplinas();
   };
 
   return (
     <div className="container">
       <h1 className="title-header">Lista de Disciplinas</h1>
       <ul className="container-lista lista-scroll">
-        {Object.values(disciplinas.disciplinasLista || {}).map((disciplina) => (
+        {disciplinas.map((disciplina) => (
           <li className="lista-disciplinas" key={disciplina.id_discip}>
             {disciplina.nm_disciplina}
             <div className="buttons-lista">
@@ -210,7 +157,7 @@ const ListarDisciplinas = () => {
                 className="botao-salvar-modal"
                 onClick={
                   selectedDisciplinas
-                    ? () => handleSalvarEdicao(selectedDisciplinas.id_discip)
+                    ? handleSalvarEdicao
                     : handleCadastrar
                 }
               >
@@ -230,12 +177,50 @@ const ListarDisciplinas = () => {
             <div className="button-grupo-modal">
               <button
                 className="botao-salvar-modal"
-                onClick={() => handleExcluir(deleteId)}
+                onClick={() => handleExcluir(selectedDisciplinas)}
               >
                 Sim
               </button>
               <button onClick={closeModal} className="botao-fechar-modal">
                 Não
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      {isEditModalOpen && (
+        <div className="modal-background">
+          <div className="modal">
+            <h2>Editar Disciplina</h2>
+            <div className="input-grupo-modal">
+              <input
+                type="text"
+                placeholder="Nome"
+                value={newDisciplina}
+                onChange={handleNameChange}
+              />
+              <input
+                type="text"
+                placeholder="Quantidade de dias"
+                value={newQtdDias}
+                onChange={handleQtdDias}
+              />
+              <input
+                type="text"
+                placeholder="Número da Fase"
+                value={newNumFase}
+                onChange={handleNumFase}
+              />
+            </div>
+            <div className="button-grupo-modal">
+              <button
+                className="botao-salvar-modal"
+                onClick={handleSalvarEdicao}
+              >
+                Salvar
+              </button>
+              <button onClick={closeModal} className="botao-fechar-modal">
+                Fechar
               </button>
             </div>
           </div>
--- intern_software/src/Components/ListarTurmas.jsx
+++ intern_software/src/Components/ListarTurmas.jsx
@@ -1,164 +1,103 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import "../style/listarTurmas.css";
 
 const ListarTurmas = () => {
-  const [turmas, setTurmas] = useState([]);
+  const [turmas, setTurmas] = useState([
+    { id_turma: 1, nm_turma: "Turma A", qtd_alunos: 30 },
+    { id_turma: 2, nm_turma: "Turma B", qtd_alunos: 25 },
+    // Adicione mais turmas conforme necessário
+  ]);
+
   const [newTurma, setNewTurma] = useState("");
   const [newQtdAlunos, setNewQtdAlunos] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTurmas, setSelectedTurmas] = useState(null);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-  const [deleteId, setDeleteId] = useState("");
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
-  useEffect(() => {
-    fetchTurmas();
-  }, []);
-
-  const fetchTurmas = async () => {
-    try {
-      let api = `https://senai-back-end.onrender.com/turmas/lista`;
-      let response = await fetch(api);
-      const data = await response.json();
-      setTurmas(data);
-    } catch (error) {
-      console.error("Deu ruim: ", error);
-    }
-  };
-
-  //Modal de cadastro e edição
   const openModal = () => {
     setIsModalOpen(true);
   };
+
   const closeModal = () => {
     setIsModalOpen(false);
     setSelectedTurmas(null);
     setNewTurma("");
     setNewQtdAlunos("");
     setIsDeleteModalOpen(false);
+    setIsEditModalOpen(false);
   };
-  //Modal de Delete
+
   const openDeleteModal = (idDelete) => {
     setIsDeleteModalOpen(true);
-    setDeleteId(idDelete);
+    setSelectedTurmas(idDelete);
+  };
+
+  const openEditModal = (turma) => {
+    setSelectedTurmas(turma.id_turma);
+    setNewTurma(turma.nm_turma);
+    setNewQtdAlunos(turma.qtd_alunos);
+    setIsEditModalOpen(true);
   };
 
   const handleNameChange = (e) => {
     setNewTurma(e.target.value);
   };
+
   const handleQtdAlunos = (e) => {
     setNewQtdAlunos(e.target.value);
   };
 
-  //CADASTRO (POST)
-  const handleCadastrar = async () => {
-    const fetchData = async () => {
-      //Gerador de ID que busca o ID mais alto, e adciona +1
-      const idMaisAlto = Math.max(
-        ...turmas.turmasLista.map((turma) => turma.id_turma)
-      );
-      let id_creator = idMaisAlto + 1;
-
-      if (id_creator <= 99999) {
-        try {
-          let api = `https://senai-back-end.onrender.com/turmas/postar`;
-          const body = {
-            id_turma: id_creator,
-            nm_turma: `${newTurma}`,
-            qtd_alunos: newQtdAlunos,
-          };
-          let response = await fetch(api, {
-            method: "POST",
-            body: JSON.stringify(body),
-            headers: {
-              "Content-type": "application/json; charset=UTF-8",
-            },
-          });
-          const data = await response.json();
-          setTurmas(data);
-        } catch (error) {
-          console.error("Deu ruim: ", error);
-        }
-      } else {
-        console.error("Unable to find a unique ID within the 5-digit limit.");
-      }
-    };
-
-    await fetchData();
-    closeModal();
-    await fetchTurmas();
+  const handleCadastrar = () => {
+    if (newTurma && newQtdAlunos) {
+      const newTurmaObject = {
+        id_turma: turmas.length + 1,
+        nm_turma: newTurma,
+        qtd_alunos: parseInt(newQtdAlunos),
+      };
+      setTurmas([...turmas, newTurmaObject]);
+      closeModal();
+    }
   };
 
-  //EXCLUSÃO (DELETE)
-  const handleExcluir = async (id_turma) => {
-    const fetchData = async () => {
-      try {
-        let api = `https://senai-back-end.onrender.com/turmas/deletar/${id_turma}`;
-        let response = await fetch(api, { method: "DELETE" });
-        await response.json();
-      } catch (error) {
-        console.error("Deu ruim: ", error);
-      }
-    };
-
-    await fetchData();
-    await fetchTurmas();
+  const handleExcluir = () => {
+    const updatedTurmas = turmas.filter((turma) => turma.id_turma !== selectedTurmas);
+    setTurmas(updatedTurmas);
     closeModal();
   };
 
-  //EDIÇÃO (PUT)
   const handleEditar = (turma) => {
-    setSelectedTurmas(turma);
-    setNewTurma(turma.nm_turma);
-    setNewQtdAlunos(turma.qtd_alunos);
-    openModal();
+    openEditModal(turma);
   };
 
-  const handleSalvarEdicao = async (id_turma) => {
-    const fetchData = async () => {
-      try {
-        let api = `https://senai-back-end.onrender.com/turmas/atualizar/${id_turma}`;
-        let response = await fetch(api, {
-          method: "PUT",
-          body: JSON.stringify({
-            nm_turma: `${newTurma}`,
-            qtd_alunos: newQtdAlunos,
-          }),
-          headers: {
-            "Content-type": "application/json; charset=UTF-8",
-          },
-        });
-        const data = await response.json();
-        setTurmas(data);
-      } catch (error) {
-        console.error("Deu ruim: ", error);
+  const handleSalvarEdicao = () => {
+    const updatedTurmas = turmas.map((turma) => {
+      if (turma.id_turma === selectedTurmas) {
+        return {
+          ...turma,
+          nm_turma: newTurma,
+          qtd_alunos: parseInt(newQtdAlunos),
+        };
       }
-    };
-
-    await fetchData();
+      return turma;
+    });
+    setTurmas(updatedTurmas);
     closeModal();
-    await fetchTurmas();
   };
-  
 
   return (
     <div className="container">
       <h1 className="title-header">Lista de Turmas</h1>
       <ul className="container-lista lista-scroll">
-        {Object.values(turmas.turmasLista || {}).map((turma) => (
+        {turmas.map((turma) => (
           <li className="lista-turma" key={turma.id_turma}>
             {turma.nm_turma}
             <div className="buttons-lista">
-              <button
-                className="button-editar"
-                onClick={() => handleEditar(turma)}
-              >
+              <button className="button-editar" onClick={() => handleEditar(turma)}>
                 <FaEdit />
               </button>
-              <button
-                className="button-excluir"
-                onClick={() => openDeleteModal(turma.id_turma)}
-              >
+              <button className="button-excluir" onClick={() => openDeleteModal(turma.id_turma)}>
                 <FaTrash />
               </button>
             </div>
@@ -189,11 +128,7 @@ const ListarTurmas = () => {
             <div className="button-grupo-modal">
               <button
                 className="botao-salvar-modal"
-                onClick={
-                  selectedTurmas
-                    ? () => handleSalvarEdicao(selectedTurmas.id_turma)
-                    : handleCadastrar
-                }
+                onClick={selectedTurmas ? handleSalvarEdicao : handleCadastrar}
               >
                 {selectedTurmas ? "Salvar" : "Cadastrar"}
               </button>
@@ -209,14 +144,40 @@ const ListarTurmas = () => {
           <div className="modal">
             <h2>Tem certeza que deseja excluir?</h2>
             <div className="button-grupo-modal">
-              <button
-                className="botao-salvar-modal"
-                onClick={() => handleExcluir(deleteId)}
-              >
+              <button className="botao-salvar-modal" onClick={handleExcluir}>
                 Sim
               </button>
               <button onClick={closeModal} className="botao-fechar-modal">
                 Não
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      {isEditModalOpen && (
+        <div className="modal-background">
+          <div className="modal">
+            <h2>Editar Turma</h2>
+            <div className="input-grupo-modal">
+              <input
+                type="text"
+                placeholder="Nome"
+                value={newTurma}
+                onChange={handleNameChange}
+              />
+              <input
+                type="text"
+                placeholder="Quantidades de alunos"
+                value={newQtdAlunos}
+                onChange={handleQtdAlunos}
+              />
+            </div>
+            <div className="button-grupo-modal">
+              <button className="botao-salvar-modal" onClick={handleSalvarEdicao}>
+                Salvar
+              </button>
+              <button onClick={closeModal} className="botao-fechar-modal">
+                Fechar
               </button>
             </div>
           </div>
